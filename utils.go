031009a1e5080044e375e241df73a09cf338d983
Rye Miller <millerrye17@gmail.com>
2021-01-02 20:36:01 -0700

    Addition of Random function to generate a random integer and print it.

--- utils.go
+++ utils.go
@@ -1,12 +1,25 @@
 package util
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 /*
-Average Calculates the average of several numbers.
-*/
+Average Calculates the average of several numbers. */
 func Average(numbers ...float64) float64 {
 	var sum float64 = 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum / float64(len(numbers))
+}
+
+/*
+Random Generates a random number. */
+func Random() {
+	rand.Seed(time.Now().Unix())
+	x := rand.Intn(7)
+	fmt.Println(x)
 }
