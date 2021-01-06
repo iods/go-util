d456a371bc62ea8f7eaa2183053619616e738129
Rye Miller <millerrye17@gmail.com>
2021-01-05 19:15:09 -0700

    Addition of the Date struct and methods with calendar package.

---
+++ calendar/date.go
@@ -0,0 +1,57 @@
+package calendar
+
+import "errors"
+
+type Date struct {
+	year  int
+	month int
+	day   int
+}
+
+/*
+Year Getter method to return the year value of the Date struct. */
+func (d *Date) Year() int {
+	return d.year
+}
+
+/*
+Month Getter method to return the month value of the Date struct. */
+func (d *Date) Month() int {
+	return d.month
+}
+
+/*
+Day Getter method to return the day value of the Date struct. */
+func (d *Date) Day() int {
+	return d.day
+}
+
+/*
+SetYear Sets the year value of the Date struct. */
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
+	d.year = year
+	return nil
+}
+
+/*
+SetMonth Sets the month value of the Date struct. */
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
+	d.month = month
+	return nil
+}
+
+/*
+SetDay Sets the day value of the Date struct. */
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("invalid day")
+	}
+	d.day = day
+	return nil
+}
---
+++ calendar/date_test.go
@@ -0,0 +1,48 @@
+package calendar
+
+import "testing"
+
+func TestDate_Year(t *testing.T) {
+	d := Date{year: 1988}
+	if d.year != 1988 {
+		t.Error("the year entered does not equal 1988.")
+	}
+}
+
+func TestDate_SetYear(t *testing.T) {
+	d := Date{}
+	err := d.SetYear(1988)
+	if err != nil && d.year < 1 {
+		t.Error("0 is not a valid year")
+	}
+}
+
+func TestDate_Month(t *testing.T) {
+	d := Date{month: 04}
+	if d.month != 04 {
+		t.Error("the month entered was not what was returned")
+	}
+}
+
+func TestDate_SetMonth(t *testing.T) {
+	d := Date{}
+	err := d.SetMonth(04)
+	if err != nil && d.month < 1 || d.month > 12 {
+		t.Error("the month set must be between 1 and 12")
+	}
+}
+
+func TestDate_Day(t *testing.T) {
+	d := Date{day: 25}
+	if d.day != 25 {
+		t.Error("the day entered does not equal 25")
+	}
+}
+
+func TestDate_SetDay(t *testing.T) {
+	d := Date{}
+	err := d.SetDay(25)
+	if err != nil && d.day < 1 || d.day > 31 {
+		t.Error("the day set must be between 1 and 31")
+	}
+}
