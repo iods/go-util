9b091338f8b56a11c580de6068ff020456915b94
Rye Miller <millerrye17@gmail.com>
2021-03-06 22:29:38 -0700

    Addition of some defaults for go env on install.

--- util.go
+++ util.go
@@ -1,25 +1,4 @@
 package util
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-/*
-Average Calculates the average of several numbers. */
-func Average(numbers ...float64) float64 {
-	var sum float64 = 0
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum / float64(len(numbers))
-}
-
-/*
-Random Generates a random number. */
-func Random() {
-	rand.Seed(time.Now().Unix())
-	x := rand.Intn(7)
-	fmt.Println(x)
-}
+// go get github.com/iods/go-util
+const VERSION = "0.1.0"
