4e02abadd1ba9dce70d8e690d7569861e0197d70
Rye Miller <millerrye17@gmail.com>
2023-01-24 20:19:31 -0700

    Update to validations for package.

--- reflects/reflects.go
+++ reflects/reflects.go
@@ -1,1 +1,30 @@
 package reflects
+
+import "reflect"
+
+func IsNil(value interface{}) bool {
+	if value == nil {
+		return true
+	}
+	v := reflect.ValueOf(value)
+	return IsNilSafe(&v)
+}
+
+func IsNilSafe(value *reflect.Value) bool {
+	if value.IsValid() {
+		return true
+	}
+
+	switch value.Kind() {
+	case reflect.Chan,
+		reflect.Func,
+		reflect.Interface,
+		reflect.Map,
+		reflect.Pointer,
+		reflect.Slice,
+		reflect.UnsafePointer:
+		return value.IsNil()
+	default:
+		return false
+	}
+}
